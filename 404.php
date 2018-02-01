649de4698221aded9290c4b12928ca70914cbc52
xiaoz <xiaoz93@outlook.com>
2018-02-01 17:06:50 +0800

    优化

--- 404.php
+++ 404.php
@@ -7,12 +7,24 @@
 			<!--post-->
 			<div class="layui-col-md8">
 				<div class="error-page">
-		            <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
+		            <h2 class="post-title"><?php _e('抱歉，页面没找到'); ?></h2>
+		            <p><i class="layui-icon" style="font-size: 20em; color: #1E9FFF;">&#xe61c;</i></p>
 		            <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
-		            <form method="post">
+		            <hr>
+		            <!--<form method="post">
 		                <p><input type="text" name="s" class="text" autofocus /></p>
 		                <p><button type="submit" class="submit"><?php _e('搜索'); ?></button></p>
-		            </form>
+		            </form>-->
+		            <form method="post" class="layui-form">
+			            <div class="layui-form-item">
+						   <table width = "100%;">
+							    <tr>
+								    <td width = "80%;"><input type="text" name="s" class="layui-input" placeholder="回车搜索" style = "border-right:0;" /></td>
+								    <td width = "20%;"><button class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button></td>
+							    </tr>
+						   </table>
+						</div>
+					</form>
 		        </div>
 			</div>
 			<!--post end-->
